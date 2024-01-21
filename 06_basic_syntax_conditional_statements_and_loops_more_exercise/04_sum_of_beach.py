079b70b821a30711e90bd56e4feb55ef46ac3810
DamyanKoykov <147879727+DamyanKoykov@users.noreply.github.com>
2024-01-21 19:36:53 +0000

    Update 04_sum_of_beach.py

--- 06_basic_syntax_conditional_statements_and_loops_more_exercise/04_sum_of_beach.py
+++ 06_basic_syntax_conditional_statements_and_loops_more_exercise/04_sum_of_beach.py
@@ -3,8 +3,8 @@ word = input().lower()
 total_count = 0
 search_for = ["sand", "water", "fish", "sun"]
 
-for list_index in range(len(search_for)):
-    counter = word.count(search_for[list_index])
+for current_word in search_for:
+    counter = word.count(current_word)
     if counter > 0:
         total_count += counter
 print(total_count)
